5f4bf278432f8093185f21efebd8b95530fcd596
rflorian2017 <roby_florian@yahoo.com>
2020-04-26 10:52:26 +0300

    added schema

--- TODOApp/target/classes/META-INF/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `users` (`user_id`,`password`,`username`) VALUES (1,'456','John');
-
-INSERT INTO `task` (`task_id`,`created_at`,`description`,`in_progress`,`project_project_id`,`user_user_id`) VALUES (1,'2020-04-26 09:55:07.365000','A simple to do',FALSE,NULL,1);
-INSERT INTO `task` (`task_id`,`created_at`,`description`,`in_progress`,`project_project_id`,`user_user_id`) VALUES (2,'2020-04-26 09:57:03.197000','another 1',TRUE,NULL,1);
-INSERT INTO `task` (`task_id`,`created_at`,`description`,`in_progress`,`project_project_id`,`user_user_id`) VALUES (3,'2020-04-26 09:57:06.413000','...',FALSE,NULL,1);
-INSERT INTO `task` (`task_id`,`created_at`,`description`,`in_progress`,`project_project_id`,`user_user_id`) VALUES (4,'2020-04-26 09:57:09.014000','potatoes',FALSE,NULL,1);
